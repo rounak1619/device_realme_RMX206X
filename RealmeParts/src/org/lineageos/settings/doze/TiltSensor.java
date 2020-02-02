34f2558d06335024fe2d54aae29d65c598e9426a
Nauval Rizky <enuma.alrizky@gmail.com>
2020-02-03 01:53:35 +0900

    x2: parts: raise2wake: check proximity before wakeup
    * to avoid having unnecessary wakeups when phone inside the pocket
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- RealmeParts/src/org/lineageos/settings/doze/TiltSensor.java
+++ RealmeParts/src/org/lineageos/settings/doze/TiltSensor.java
@@ -48,12 +48,16 @@ public class TiltSensor implements SensorEventListener {
     private PowerManager mPowerManager;
     private WakeLock mWakeLock;
 
+    private Sensor mProximitySensor;
+    private boolean mInsidePocket = false;
+
     private long mEntryTimestamp;
 
     public TiltSensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
+        mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
         mExecutorService = Executors.newSingleThreadExecutor();
@@ -75,7 +79,11 @@ public class TiltSensor implements SensorEventListener {
             mEntryTimestamp = SystemClock.elapsedRealtime();
         }
 
-        if (event.values[0] == 0) {
+        if (!isRaiseToWake && !DozeUtils.isPocketGestureEnabled(mContext)) {
+            mInsidePocket = false;
+        }
+
+        if (event.values[0] == 0 && !mInsidePocket) {
             if (isRaiseToWake) {
                 mWakeLock.acquire(WAKELOCK_TIMEOUT_MS);
                 mPowerManager.wakeUp(SystemClock.uptimeMillis(),
@@ -91,11 +99,27 @@ public class TiltSensor implements SensorEventListener {
         /* Empty */
     }
 
+    private SensorEventListener mProximityListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            mInsidePocket = event.values[0] < mProximitySensor.getMaximumRange();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // stub
+        }
+    };
+
     protected void enable() {
         if (DEBUG) Log.d(TAG, "Enabling");
         submit(() -> {
             mSensorManager.registerListener(this, mSensor,
                     SensorManager.SENSOR_DELAY_NORMAL, BATCH_LATENCY_IN_MS * 1000);
+            if (DozeUtils.isRaiseToWakeEnabled(mContext)) {
+                mSensorManager.registerListener(mProximityListener, mProximitySensor,
+                        SensorManager.SENSOR_DELAY_NORMAL);
+            }
             mEntryTimestamp = SystemClock.elapsedRealtime();
         });
     }
@@ -104,6 +128,9 @@ public class TiltSensor implements SensorEventListener {
         if (DEBUG) Log.d(TAG, "Disabling");
         submit(() -> {
             mSensorManager.unregisterListener(this, mSensor);
+            if (DozeUtils.isRaiseToWakeEnabled(mContext)) {
+                mSensorManager.unregisterListener(mProximityListener, mProximitySensor);
+            }
         });
     }
 }
