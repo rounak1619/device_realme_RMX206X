635ed35626b384eaa9011f9d0d6150eb33dc3011
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-09-26 22:03:24 +0530

    RMX206X: set correct props fix ok google in screen off state
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- init/init_rmx206x.cpp
+++ init/init_rmx206x.cpp
@@ -25,24 +25,36 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <fstream>
+
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
 #include <android-base/properties.h>
+#include <android-base/logging.h>
+
 #include "property_service.h"
 #include "vendor_init.h"
 
 using android::init::property_set;
 
-void property_override(char const prop[], char const value[])
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
+};
+
+void property_override(char const prop[], char const value[], bool add = true)
 {
     prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
+    pi = (prop_info *)__system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
-    else
+    else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
@@ -53,6 +65,26 @@ void property_override_prop(char const system_prop[], char const value[])
 
 void vendor_load_properties()
 {
+    const auto set_ro_build_prop = [](const std::string &source,
+                                      const std::string &prop,
+                                      const std::string &value) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    const auto set_ro_product_prop = [](const std::string &source,
+                                        const std::string &prop,
+                                        const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    for (const auto &source : ro_props_default_source_order)
+    {
+        set_ro_product_prop(source, "device", "RMX2061L1");
+        set_ro_product_prop(source, "model", "RMX2061");
+        set_ro_product_prop(source, "name", "RMX2061");
+    }
     // fingerprint
     property_override("ro.build.description", "coral-user 10 RP1A.200720.009 6720564 release-keys");
     property_override_prop("ro.build.fingerprint", "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys");
