c121388c105023ef39e48c6b2a40b5a8341a1bda
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-10-18 22:41:00 +0530

    RMX206X: Don't build custom sensor
    Proximity and all sensors working better on stock sensor than this
    
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- device.mk
+++ device.mk
@@ -155,15 +155,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
-# Sensors Hal
-PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl.rmx206x
-
-# Sensors
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
-
 # Ril
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4
