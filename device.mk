477e8aeaaa89648ed23f5c781fb6f3ba501fad10
PIPIPIG233666 <pig.priv@gmail.com>
2020-11-01 17:51:18 -0500

    RMX206X: Split component-overrides to support pure AOSP/QTI BT stack
    Change-Id: Ia40a996ba5164ba8a6fc5dde0dfb3ef859af1477
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Component overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
