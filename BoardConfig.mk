e0125126d6931e8ed7f1614ac7a17294174434fd
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2021-01-10 08:14:11 +0530

    Revert "RMX206X: Include QSSI sepolicy"
    This reverts commit d6a4a78e9a24de45f26ff0804e6b75f5d5ec38c4.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,8 +185,13 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
-include device/qcom/sepolicy/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/private \
+    device/qcom/sepolicy/qva/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/public \
+    device/qcom/sepolicy/qva/public
 
 SELINUX_IGNORE_NEVERALLOWS := true
 
